6b896cf090840d52e31272b339f334f7af645d3f
Rumena Staneva <rumenastaneva2106@gmail.com>
2026-01-22 16:08:03 +0200

    updated configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import AppLayout from './components/layout/AppLayout'
 import HomePage from './pages/HomePage'
 import SwapPage from './pages/SwapPage'
